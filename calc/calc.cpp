fae6fe3d6ea03c018d6c298c672f85adc52a67ee
GeoTheo06 <theodorgeor@gmail.com>
2022-06-06 02:05:41 +0300

    Didn't change anything really...

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -2,37 +2,32 @@
 using namespace std;
 
 int main() {
-    cout << endl << "Symbola: + - * / ^" << endl << "an thes riza grapse \"!1\" meta ton arithmo" << endl;
-    while (5 > 4) {
+	cout << endl << "Symbola: + - * / ^" << endl << "an thes riza grapse \"!1\" meta ton arithmo" << endl;
+	while (5 > 4) {
 
-        double number1 = 0;
-        char symbol = '-';
-        double number2 = 0;
-        double apotelesma = 0;
+		double number1 = 0;
+		char symbol = '-';
+		double number2 = 0;
+		double apotelesma = 0;
 
-        cin >> number1 >> symbol >> number2;
+		cin >> number1 >> symbol >> number2;
 
-        if (symbol == '!') {
-            cout << "= " << sqrt(number1) << endl << endl;
-        }
-        else {
-            if (symbol == '+') {
-                apotelesma = number1 + number2;
-            }
-            else if (symbol == '-') {
-                apotelesma = number1 - number2;
-            }
-            else if (symbol == '*') {
-                apotelesma = number1 * number2;
-            }
-            else if (symbol == '/') {
-                apotelesma = number1 / number2;
-            }
-            else if (symbol == '^') {
-                apotelesma = pow(number1, number2);
-            }
+		if (symbol == '!') {
+			cout << "= " << sqrt(number1) << endl << endl;
+		} else {
+			if (symbol == '+') {
+				apotelesma = number1 + number2;
+			} else if (symbol == '-') {
+				apotelesma = number1 - number2;
+			} else if (symbol == '*') {
+				apotelesma = number1 * number2;
+			} else if (symbol == '/') {
+				apotelesma = number1 / number2;
+			} else if (symbol == '^') {
+				apotelesma = pow(number1, number2);
+			}
 
-            cout << "= " << apotelesma << endl << endl;
-        }
-    }
+			cout << "= " << apotelesma << endl << endl;
+		}
+	}
 }
