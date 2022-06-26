ecabd8bde77621ecd13a3868547e15428c9578c7
GeoTheo06 <theodorgeor@gmail.com>
2022-06-26 23:54:17 +0300

    Started working on Encryption - decryption 2.

--- Encyption - Decryption 2.0/Encyption - Decryption 2.0.cpp
+++ Encyption - Decryption 2.0/Encyption - Decryption 2.0.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-//apo to ena gramma tou xrhsth esy bgazeis alla 4. Prepei na vreis tropo na vriskei h apokryptografhsh  afto to gramma xrhsimopoiontas ta 4
 
-string privateKey =
+string privateKey = "a";
 
 void encryption() {
 	cout << "Please enter text to be encrypted: ";
