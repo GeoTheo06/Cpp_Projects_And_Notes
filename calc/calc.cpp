ef73ca22f9d36e5bfa66f9533f3a20d056e37a7e
GeoTheo06 <theodorgeor@gmail.com>
2022-10-08 20:25:38 +0300

    previous commit is not working, this is better

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -2,34 +2,34 @@
 using namespace std;
 
 int main() {
-	cout << endl << "Symbola: + - * / ^" << endl;
+	cout << endl << "Symbols: + - * / ^ v (v for roots)" << endl;
 	while (5 > 4) {
 
 		double number1 = 0;
 		char symbol = '-';
 		double number2 = 0;
-		double apotelesma = 0;
+		double outcome = 0;
 
 		cin >> number1 >> symbol >> number2;
 
 		int number1Int = number1;
 		int number2Int = number2;
 
-		if (symbol == '^') {
-			apotelesma = number1Int ^ number2Int;
+		if (symbol == 'v') {
+			outcome = pow(number2, 1 / number1);
 		} else if (symbol == '+') {
-			apotelesma = number1 + number2;
+			outcome = number1 + number2;
 		} else if (symbol == '-') {
-			apotelesma = number1 - number2;
+			outcome = number1 - number2;
 		} else if (symbol == '*') {
-			apotelesma = number1 * number2;
+			outcome = number1 * number2;
 		} else if (symbol == '/') {
-			apotelesma = number1 / number2;
+			outcome = number1 / number2;
 		} else if (symbol == '^') {
-			apotelesma = pow(number1, number2);
+			outcome = pow(number1, number2);
 		}
 
-		cout << "= " << apotelesma << endl << endl;
+		cout << "= " << outcome << endl << endl;
 
 	}
 }
