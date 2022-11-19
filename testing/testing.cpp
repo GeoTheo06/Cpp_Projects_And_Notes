b7f71ad5a3e8fff1a9d9ff43247db818b5abea77
GeoTheo06 <theodorgeor@gmail.com>
2022-11-19 14:30:44 +0200

    not important stuff

--- testing/testing.cpp
+++ testing/testing.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
-#include <windows.h>
-#include <ctime>
-
+#include <string>
+#include<iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-	srand(time(0)); //true random number generation
+void findOdd(vector<int> numbers) {
+	int counter = 0;
+	for (int i = 0; i < numbers.size() - 1; i++) {
+		counter = 0;
+		for (int k = 0; k < numbers.size() - 1; k++) {
+			if (numbers[i] == numbers[k]) {
+				counter++;
+			}
+		}
+		if (counter % 2 == 1) {
+			cout << numbers[i];
+		}
+	}
+}
 
+int main() {
+	findOdd({ 1, 2, 1, 5 });
 }
